69da987330cb8bac6bfb8144c3d607783f30b6a3
845938783 <845938783@qq.com>
2021-03-20 14:37:50 +0800

    小程序组件的练习

---
+++ pages/input/input.js
@@ -0,0 +1,74 @@
+// pages/input/input.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  handleInput(event){
+    console.log('用户输入内容', event);
+  },
+  handleFoucs(event){
+    console.log('input获取焦点',event);
+  },
+  bindBlur(event){
+    console.log('input失去焦点',event);
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
